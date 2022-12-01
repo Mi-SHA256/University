6639f149d24bf66cef602e6726cf43ffd273a9a2
Misha <115937346+MishaTheBalkan@users.noreply.github.com>
2022-12-01 13:57:37 +0000

    Add files via upload

--- Netmiko_Config.py
+++ Netmiko_Config.py
@@ -16,12 +16,21 @@ device1 = {
     'secret': secret,    
 }
 
-connection = connectHandler(**device1)
+connection = ConnectHandler(**device1)
 connection.enable()
 
-#the commands below are for the router that was running on the IP address of 192.168.56.130 on NETLAB
-commands = ["enable","conf t","int lo1","ip add 1.1.1.1 255.255.255.255","exit","router ospf 1","network 1.1.1.1 255.255.255.0 area 0","network 2.2.2.2 255.255.255.0 area 0","network 192.168.56.103 255.255.255.0 area 0","exit","router eigrp 1","network 192.168.56.0","end","wr","exit"]
-connection.send_config_set(commands)
+#the commands below are for the router R2 that was running on the IP address of 192.168.56.130 on NETLAB
+commands1 = ["int lo1","ip add 1.1.1.1 255.255.255.255"]
+commands2 = ["router ospf 1","network 1.1.1.1 255.255.255.0 area 0","network 2.2.2.2 255.255.255.0 area 0","network 192.168.56.130 255.255.255.0 area 0",]
+commands3 = ["router eigrp 1","network 192.168.56.0"]
+
+#for some reason netmiko dislikes using the command "exit" 
+#so because netmiko automatically places the user in configure-terminal
+#I found it more comfortable to reset the position after being done with an interface
+#rather than finding a way of exiting the interface without crashing the program
+connection.send_config_set(commands1)
+connection.send_config_set(commands2)
+connection.send_config_set(commands3)
 
 r = connection.send_command("show int desc")
 
